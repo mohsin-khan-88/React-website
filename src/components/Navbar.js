aae3f77d256f644fad850afec826b18f730d94dd
Mohsin Khan <mhbijarani@gmail.com>
2021-10-15 16:23:44 -0400

    Added navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
 
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
     
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
     return (
         <>
-        <nav className="navbar">
+            <nav className="navbar">
             <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
-                TRVL <i class="fab fa-typo3" />
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                TRVL <i className="fab fa-typo3" />
             </Link>
             <div className="menu-icon" onClick={handleClick}>
-                <i className={click ? 'fas fa-times' : 'fas-fa-bars'} />
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
             <ul className={click ? 'nav-menu active' :  'nav-menu'}>
                 <li className="nav-item">
@@ -56,6 +73,7 @@ function Navbar() {
                     </Link>
                 </li>
             </ul>
+            {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </div>
         </nav>
         </>
